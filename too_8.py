c5d1b0f37b3dca071fb87667eb132827f25d940c
Heidi Lees <heidi_lees@hotmail.com>
2015-04-24 15:55:58 +0300

    Lisasin kodutoo 8

---
+++ too_8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+__author__ = "Heidi Lees"
+__copyright__ = "Copyright 2015, Minufirma"
+__credits__ = ["Heidi Lees", "Taaniel Jakobson", "Tiina Aid"]
+__version__ = "0.001"
+__email__ = "heidi_lees@hotmail.com"
+
+
+import os
+from sys import argv
+from math import sqrt
+
+def main():
+	def ruutjuur(n):
+		return sqrt(n)
+	print("arvu n ruutjuur")
+	n = int(input())
+	ruutjuur(n)
+
+	text_file = open("Vastus.txt", "w")
+	text_file.write(str(sqrt(n)))
+	text_file.close()
+
+if __name__ == "__main__":
+	main()
+	
+print
